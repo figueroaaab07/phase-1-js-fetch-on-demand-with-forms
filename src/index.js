ff6ddc6be7a9de9f8098936ac4f0107a638b3843
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-05-06 17:16:43 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -5,22 +5,43 @@ const init = () => {
     // console.log(event.target.children[1].value);
     const input = document.querySelector('input#searchByID');
     // console.log(input.value);
-    if (input.value >= 1 && input.value <= 3) {
-      fetch(`http://localhost:3000/movies/${input.value}`)
-      .then(response => response.json())
-      .then(data => {
-        // console.log(data);
-        const title = document.querySelector('section#movieDetails h4');
-        const summary = document.querySelector('section#movieDetails p');
-        title.innerText = data.title;
-        summary.innerText = data.summary;
-      })
-    } else {
+    // if (input.value >= 1 && input.value <= 3) {
+    //   fetch(`http://localhost:3000/movies/${input.value}`)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     // console.log(data);
+    //     const title = document.querySelector('section#movieDetails h4');
+    //     const summary = document.querySelector('section#movieDetails p');
+    //     title.innerText = data.title;
+    //     summary.innerText = data.summary;
+    //   })
+    // } else {
+    //   const title = document.querySelector('section#movieDetails h4');
+    //   const summary = document.querySelector('section#movieDetails p');
+    //   title.innerText = 'Please introduce a valid Movie ID (1 - 3)';
+    //   summary.innerText = '';
+    // }
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Status code error: " + response.status);
+      }
+    })
+    .then(data => {
       const title = document.querySelector('section#movieDetails h4');
       const summary = document.querySelector('section#movieDetails p');
-      title.innerText = 'Please introduce un valid ID (1 - 3)';
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    })
+    .catch(error => {
+      // console.log(error);
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+      title.innerText = `${error}. Please introduce a valid Movie ID (1 - 3)`;
       summary.innerText = '';
-    }
+    })
   })
 }
 
